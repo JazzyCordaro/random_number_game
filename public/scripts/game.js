31ee6fd65e8535bbbd648d45bd9984d28e487135
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-08 17:34:54 -0500

    end of class

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -2,24 +2,28 @@ console.log('game.js sourced');
 
 //global variables
 var players = [];
+var numPlayers = 0;
 var guessCounter = 0;
-var setup = $('#setup');
+var gameDifficulty = '';
 
-var remove = function () {
-  setup.remove();
-};
+console.log('numPlayers:', numPlayers);
+
 
 $(document).ready(function () {
-  console.log('jquery works');
+  //capture information
+  $('#rollDice').on('click', function () {
+    console.log('in rollDice click');
+    numPlayers = $('#numPlayers').val();
+    gameDifficulty = $('#difficulty').val();
+    $('#setup').slideUp(1000, function () {
+      $('#setup').remove();
+      console.log('numPlayers:', numPlayers);
+      console.log('gameDifficulty:', gameDifficulty);
+    });
 
 
-$('#rollDice').on('click', function () {
-  console.log('in rollDice click');
-  setup.slideUp(1000, remove);
-
-  //click go and initiate game
-  //hide the #setup div
-  //display #playMode
-});//en rollDice click
-
+    //click go and initiate game
+    //hide the #setup div
+    //display #playMode
+  });//en rollDice click
 });//end doc ready
