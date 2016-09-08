5e4a7ba5c07e04330c07f8bad4623a9b155b4c60
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-08 16:46:33 -0500

    source fix

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,1 +1,25 @@
 console.log('game.js sourced');
+
+//global variables
+var players = [];
+var guessCounter = 0;
+var setup = $('#setup');
+
+var remove = function () {
+  setup.remove();
+};
+
+$(document).ready(function () {
+  console.log('jquery works');
+
+
+$('#rollDice').on('click', function () {
+  console.log('in rollDice click');
+  setup.slideUp(1000, remove);
+
+  //click go and initiate game
+  //hide the #setup div
+  //display #playMode
+});//en rollDice click
+
+});//end doc ready
