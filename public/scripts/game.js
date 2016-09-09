46f0d8987072a9c5cd1229a7b396ef787d36581b
Ross Gravem <rgravem@yahoo.com>
2016-09-09 10:11:49 -0500

    module working

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,1 +1,22 @@
 console.log('game.js sourced');
+
+$( document ).ready(function(){
+  console.log('doc ready');
+  var objectToSend = {
+    min: 1,
+    max: 10,
+    integer: true
+  };
+  $.ajax({
+    type: 'POST',
+    url: '/ranNum',
+    data: objectToSend,
+    success: function(data){
+      // console.log(data);
+    }
+  });
+
+
+
+
+}); // end doc rdy
