c677b43fcb4117cd743d407a7e7e279df572dd23
Ross Gravem <rgravem@yahoo.com>
2016-09-09 15:12:51 -0500

    returning high/low/winner

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -10,12 +10,28 @@ var objectToSend = {
   max: 10,
   integer: true
 };
-
-
+var responses ={
+  player1:'no guess made yet',
+  player2:'no guess made yet',
+  player3:'no guess made yet',
+  player4:'no guess made yet',
+};
 var player1In;
 var player2In;
 var player3In;
 var player4In;
+var playerGuesses = {
+  player1: player1In,
+  player2: player2In,
+  player3: player3In,
+  player4: player4In
+};
+
+ply1GuessCount = 0;
+ply2GuessCount = 0;
+ply3GuessCount = 0;
+ply4GuessCount = 0;
+
 
 $(document).ready(function () {
   //click go and initiate game
@@ -51,13 +67,29 @@ var selectedDifficulty = function() {
     }
   };
 
+var getPlayerGuess = function() {
+  $.ajax({
+    type: 'POST',
+    url: '/playerGuess',
+    data: playerGuesses,
+    success: function(data){
+      data.player1 = responses.player1;
+      data.player2 = responses.player2;
+      data.player3 = responses.player3;
+      data.player4 = responses.player4;
+      console.log('sent from server:', data);
+    }
+
+  });
+};
+
 var setDifficulty = function() {
   $.ajax({
     type: 'POST',
     url: '/ranNum',
     data: objectToSend,
     success: function(data){
-      console.log('sent from server:', data);
+
     }
 
   });
@@ -65,20 +97,20 @@ var setDifficulty = function() {
 
 //display #playMode
 var displayGameMode = function () {
-  console.log('in displayGameMode');
+  // console.log('in displayGameMode');
   console.log('numPlayers:', numPlayers);
   switch (numPlayers) {
     case 1:
-      $('#playMode').append('<p>Player 1</p><input id="guess1" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
+      $('#playMode').append('<p>Player 1</p><p>Total guesses: <span id="ply1GuessSpan">' + ply1GuessCount + '</span></p><input id="guess1" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
       break;
     case 2:
-      $('#playMode').append('<p>Player 1</p><input id="guess1" type="text" value="" placeholder="Guess a #"><p>Player 2</p><input id="guess2" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
+      $('#playMode').append('<p>Player 1</p><p>Total guesses: <span id="ply1GuessSpan">' + ply1GuessCount + '</span></p><input id="guess1" type="text" value="" placeholder="Guess a #"><p>Player 2</p><p>Total guesses: <span id="ply2GuessSpan">' + ply2GuessCount + '</span></p><input id="guess2" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
       break;
     case 3:
-      $('#playMode').append('<p>Player 1</p><input id="guess1" type="text" value="" placeholder="Guess a #"><p>Player 2</p><input id="guess2" type="text" value="" placeholder="Guess a #"><p>Player 3</p><input id="guess3" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
+      $('#playMode').append('<p>Player 1</p><p>Total guesses: <span id="ply1GuessSpan">' + ply1GuessCount + '</span></p><input id="guess1" type="text" value="" placeholder="Guess a #"><p>Player 2</p><p>Total guesses: <span id="ply2GuessSpan">' + ply2GuessCount + '</span></p><input id="guess2" type="text" value="" placeholder="Guess a #"><p>Player 3</p><p>Total guesses: <span id="ply3GuessSpan">' + ply3GuessCount + '</span></p><input id="guess3" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
       break;
     default:
-      $('#playMode').append('<p>Player 1</p><input id="guess1" type="text" value="" placeholder="Guess a #"><p>Player 2</p><input id="guess2" type="text" value="" placeholder="Guess a #"><p>Player 3</p><input id="guess3" type="text" value="" placeholder="Guess a #"><p>Player 4</p><input id="guess4" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
+      $('#playMode').append('<p>Player 1</p><p>Total guesses: <span id="ply1GuessSpan">' + ply1GuessCount + '</span></p><input id="guess1" type="text" value="" placeholder="Guess a #"><p>Player 2</p><p>Total guesses: <span id="ply2GuessSpan">' + ply2GuessCount + '</span></p><input id="guess2" type="text" value="" placeholder="Guess a #"><p>Player 3</p><p>Total guesses: <span id="ply3GuessSpan">' + ply3GuessCount + '</span></p><input id="guess3" type="text" value="" placeholder="Guess a #"><p>Player 4</p><p>Total guesses: <span id="ply4GuessSpan">' + ply4GuessCount + '</span></p><input id="guess4" type="text" value="" placeholder="Guess a #"><br><button id="submitGuesses">Take a guess</button>');
   } // end switch
 
   $('#submitGuesses').on('click', function () {
@@ -90,10 +122,26 @@ var displayGameMode = function () {
     console.log('player2In:', player2In);
     console.log('player3In:', player3In);
     console.log('player4In:', player4In);
+      playerGuesses.player1 = player1In;
+      playerGuesses.player2 = player2In;
+      playerGuesses.player3 = player3In;
+      playerGuesses.player4 = player4In;
+      //update the player guess count
+      ply1GuessCount++;
+      ply2GuessCount++;
+      ply3GuessCount++;
+      ply4GuessCount++;
+      console.log(ply4GuessCount);
 
+      //set the html content for each players total guesses
+      $('#ply1GuessSpan').html(ply1GuessCount);
+      $('#ply2GuessSpan').html(ply2GuessCount);
+      $('#ply3GuessSpan').html(ply3GuessCount);
+      $('#ply4GuessSpan').html(ply4GuessCount);
     $('#guess1').val('');
     $('#guess2').val('');
     $('#guess3').val('');
     $('#guess4').val('');
+    getPlayerGuess();
   });
 };// end displayGameMode
--- server/app.js
+++ server/app.js
@@ -4,6 +4,13 @@ var path=require('path');
 var bodyParser=require('body-parser');
 var urlencodedparser=bodyParser.urlencoded({extended: false});
 var randomNumber = require('../modules/randomNumber');
+var sendBackPlayers = {
+  player1: 1,
+  player2: 1,
+  player3: 1,
+  player4: 1,
+};
+var globalNum = 0;
 
 app.listen('5000', 'localhost', function(){
   console.log('server is listening on port 5000');
@@ -25,7 +32,66 @@ app.post('/ranNum', urlencodedparser, function(req, res){
   var randoNum = {
     rando: sendNum
   };
+  globalNum = sendNum;
   res.send(randoNum);
+
+});
+
+app.post('/playerGuess', urlencodedparser, function(req, res) {
+  console.log('serverside playerGuess hit', req.body);
+  var playerOne = Number(req.body.player1);
+  var playerTwo = Number(req.body.player2);
+  var playerThree = Number(req.body.player3);
+  var playerFour = Number(req.body.player4);
+
+console.log(globalNum);
+
+  var compareGuesses1 = function() {
+    if(playerOne === globalNum){
+      console.log('efffff ya!');
+      sendBackPlayers.player1 = 'winner';
+    } else if(playerOne < globalNum){
+      sendBackPlayers.player1 = 'low';
+    } else if(playerOne > globalNum){
+      sendBackPlayers.player1 = 'high';
+    }
+  };
+
+  var compareGuesses2 = function() {
+    if(playerTwo === globalNum){
+      sendBackPlayers.player2 = 'winner';
+    } else if(playerTwo < globalNum){
+      sendBackPlayers.player2 = 'low';
+    } else if(playerTwo > globalNum){
+      sendBackPlayers.player2 = 'high';
+    }
+  };
+
+  var compareGuesses3 = function() {
+    if(playerThree === globalNum){
+      sendBackPlayers.player3 = 'winner';
+    } else if(playerThree < globalNum){
+      sendBackPlayers.player3 = 'low';
+    } else if(playerThree > globalNum){
+      sendBackPlayers.player3 = 'high';
+    }
+  };
+
+  var compareGuesses4 = function() {
+    if(playerFour === globalNum){
+      sendBackPlayers.player4 = 'winner';
+    } else if(playerFour < globalNum){
+      sendBackPlayers.player4 = 'low';
+    } else if(playerFour > globalNum){
+      sendBackPlayers.player4 = 'high';
+    }
+  };
+  compareGuesses1();
+  compareGuesses2();
+  compareGuesses3();
+  compareGuesses4();
+  res.send(sendBackPlayers);
+  console.log('sendbackplayers= ', sendBackPlayers);
 
 });
 
